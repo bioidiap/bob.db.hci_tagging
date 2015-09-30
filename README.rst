936175039589acfa7d2b69f612e10075e3be2048
Andre Anjos <andre.anjos@idiap.ch>
2015-09-30 11:12:02 +0200

    [initial] Commit

---
+++ README.rst
@@ -0,0 +1,26 @@
+.. vim: set fileencoding=utf-8 :
+.. Andre Anjos <andre.anjos@idiap.ch>
+.. Wed 30 Sep 2015 11:03:49 CEST
+
+===============================================
+ Mahnob HCI-Tagging Database Interface for Bob
+===============================================
+
+This package contains an interface for the `Mahnob HCI-Tagging dataset`_
+interface. It is presently used to benchmark and test Remote
+Photo-Plethysmography algorithms.
+
+
+Installation
+------------
+
+To install this package -- alone or together with other `Packages of Bob
+<https://github.com/idiap/bob/wiki/Packages>`_ -- please read the `Installation
+Instructions <https://github.com/idiap/bob/wiki/Installation>`_.  For Bob_ to
+be able to work properly, some dependent packages are required to be installed.
+Please make sure that you have read the `Dependencies
+<https://github.com/idiap/bob/wiki/Dependencies>`_ for your operating system.
+
+
+.. _bob: https://www.idiap.ch/software/bob
+.. _manhob hci-tagging dataset: http://mahnob-db.eu/hci-tagging/
---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Andre Anjos <andre.anjos@idiap.ch>
+# Mon 13 Aug 2012 09:49:00 CEST
+
+from setuptools import setup, find_packages
+
+# Define package version
+version = open("version.txt").read().rstrip()
+
+setup(
+    name='bob.db.hci_tagging',
+    version=version,
+    description="Bob Database interface for the Mahnob HCI-Tagging database",
+    keywords=['bob', 'database', 'manhob', 'hci-tagging'],
+    url='http://gitlab.idiap.ch/biometric/bob.db.hci_tagging',
+    license='BSD',
+    author='Andre Anjos',
+    author_email='andre.anjos@idiap.ch',
+
+    long_description=open('README.rst').read(),
+
+    # This line is required for any distutils based packaging.
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    namespace_packages = [
+        'bob',
+        'bob.db',
+    ],
+
+    entry_points = {
+        'bob.db': [
+            'hci_tagging = bob.db.hci_tagging.driver:Interface',
+            ],
+      },
+
+    install_requires=[
+      'setuptools',
+      'bob.db.base',
+      ],
+
+    classifiers=[
+      'Framework :: Bob',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: BSD License',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+
+    )
