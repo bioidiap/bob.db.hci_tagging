e2da2801583237b9a52a06c6a9f135a17d62240c
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2016-01-21 09:37:49 +0100

    [protocols] changed the code so as to have protocol and subset(s) as arguments, add the protocol and data sets txt files

--- bob/db/hci_tagging/__init__.py
+++ bob/db/hci_tagging/__init__.py
@@ -22,47 +22,49 @@ class Database(object):
       self.metadata = [row for row in reader]
 
 
-  def objects(self, protocol=None):
+  def objects(self, protocol='all', subset=None):
     """Returns a list of unique :py:class:`.File` objects for the specific
     query by the user.
 
 
     Parameters:
 
-      protocol (str, optional): If set, can take the value of 'cvpr14',
-        which subselects samples used by Li et al. on their CVPR'14 paper for
-        heart-rate estimation.
+      protocol (str, optional): If set, can take the value of either 'cvpr14' or 'all'.
+        'cvpr14' subselects samples used by Li et al. on their CVPR'14 paper for
+        heart-rate estimation. If 'all' is set, the complete database is selected.
+
+      subset (str, optional): If set, it could be either 'train', 'dev' or 'test'
+        or a combination of them (i.e. a list). If not set (default), 
+        the files from all these sets are retrieved for the 'all' protocol.
+        Note that for 'cvpr14' protocol, this has no effect, since no training,
+        development and test set have been defined in this case.
+
 
 
     Returns: A list of :py:class:`File` objects.
     """
-
     if protocol in ('cvpr14',):
-      d = resource_filename(__name__, os.path.join('data', 'li_samples_cvpr14.txt'))
+      d = resource_filename(__name__, os.path.join('protocols/cvpr14', 'li_samples_cvpr14.txt'))
       with open(d, 'rt') as f: sessions = f.read().split()
       return [File(**k) for k in self.metadata if k['basedir'] in sessions]
     
-    elif protocol in ('train',):
-      d = resource_filename(__name__, os.path.join('data', 'train.txt'))
-      with open(d, 'rt') as f: sessions = f.read().split()
-      return [File(**k) for k in self.metadata if k['basedir'] in sessions]
-    
-    elif protocol in ('dev',):
-      d = resource_filename(__name__, os.path.join('data', 'dev.txt'))
-      with open(d, 'rt') as f: sessions = f.read().split()
-      return [File(**k) for k in self.metadata if k['basedir'] in sessions]
-    
-    elif protocol in ('test',):
-      d = resource_filename(__name__, os.path.join('data', 'test.txt'))
-      with open(d, 'rt') as f: sessions = f.read().split()
-      return [File(**k) for k in self.metadata if k['basedir'] in sessions]
-    
-    elif protocol in ('traindev',):
-      d = resource_filename(__name__, os.path.join('data', 'traindev.txt'))
-      with open(d, 'rt') as f: sessions = f.read().split()
-      return [File(**k) for k in self.metadata if k['basedir'] in sessions]
+    if protocol in ('all'):
 
-    elif protocol is not None:
-      raise RuntimeError('Protocol should be either "train", "dev", "traindev", "test", "cvpr14" or not set. The value %s is not valid' % protocol)
-
-    return [File(**k) for k in self.metadata]
+      if 'None' in subset:
+        return [File(**k) for k in self.metadata]
+      else:
+        files = []
+        if 'train' in subset:
+          d = resource_filename(__name__, os.path.join('protocols/all', 'train.txt'))
+          with open(d, 'rt') as f: sessions = f.read().split()
+          files += [File(**k) for k in self.metadata if k['basedir'] in sessions]
+        if 'dev' in subset:
+          d = resource_filename(__name__, os.path.join('protocols/all', 'dev.txt'))
+          with open(d, 'rt') as f: sessions = f.read().split()
+          files += [File(**k) for k in self.metadata if k['basedir'] in sessions]
+        if 'test' in subset:
+          d = resource_filename(__name__, os.path.join('protocols/all', 'test.txt'))
+          with open(d, 'rt') as f: sessions = f.read().split()
+          files += [File(**k) for k in self.metadata if k['basedir'] in sessions]
+      
+        return files
