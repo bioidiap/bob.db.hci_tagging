2d2968ec7bbea3bf7a5cae34e10ac976b3c4b309
Andre Anjos <andre.anjos@idiap.ch>
2016-12-14 11:59:58 +0100

    Fix doc building

--- bob/db/hci_tagging/__init__.py
+++ bob/db/hci_tagging/__init__.py
@@ -73,3 +73,24 @@ class Database(object):
           files += [File(**k) for k in self.metadata if k['basedir'] in sessions]
 
         return files
+
+
+# gets sphinx autodoc done right - don't remove it
+def __appropriate__(*args):
+  """Says object was actually declared here, an not on the import module.
+
+  Parameters:
+
+    *args: An iterable of objects to modify
+
+  Resolves `Sphinx referencing issues
+  <https://github.com/sphinx-doc/sphinx/issues/3048>`
+  """
+
+  for obj in args: obj.__module__ = __name__
+
+__appropriate__(
+    File,
+    )
+
+__all__ = [_ for _ in dir() if not _.startswith('_')]
